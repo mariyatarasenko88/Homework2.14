571c746fa32a513fdcde68a26ca8c717a935ad28
mariyatarasenko88 <tmv1231@yandex.ru>
2023-05-27 20:19:27 +0300

    Homework2.14 is donne

---
+++ src/test/java/homework2_14/StringListImplConstants.java
@@ -0,0 +1,13 @@
+package homework2_14;
+
+public class StringListImplConstants {
+    public static final String VALUE_1 = "Один";
+    public static final String VALUE_2 = "Два";
+    public static final String VALUE_3 = "Три";
+    public static final String VALUE_4 = "Четыре";
+    public static final String VALUE_5 = "Пять";
+    public static final String VALUE_NULL = null;
+    public static final String VALUE_NON_ELEMENT = "Не элемент";
+
+    public static final String ERROR_MESSAGE_NOT_CORRECT_INDEX = "Введен некорректный индекс";
+}
